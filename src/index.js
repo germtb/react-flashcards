99b5400ad8f13218cd039fa988645badbc7ce772
germtb <germtf@gmail.com>
2016-01-24 15:24:31 +0000

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FlashcardPanel from './components/FlashcardPanel';
+
+  // (<div> Hello WORLD </div>),
+
+ReactDOM.render(
+  <FlashcardPanel />,
+  document.getElementById('app')
+);
