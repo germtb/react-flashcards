51d1125f625419a8e55450eb9bbca2233483212e
germtb <germtf@gmail.com>
2016-02-09 22:18:28 +0000

    Connect input to store

--- src/components/FlashcardInput.js
+++ src/components/FlashcardInput.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-
+import {store} from '../index';
 var answer = '';
 
 export const FlashcardInput = React.createClass({
   mixins: [PureRenderMixin],
+  submitAnswer: function() {
+    store.dispatch({
+      type: 'SUBMIT_ANSWER',
+      answer: answer,
+    });
+    store.dispatch({
+      type: 'GET_NEXT',
+    });
+  },
+  handleChange: function (event) {
+    answer = event.target.value;
+  },
   render: function() {
     return (
       <div>
-        <input onChange={i => answer = i}/>
-        <button type="button"> Enter </button>
+        <input onChange={this.handleChange}/>
+        <button onClick={this.submitAnswer}> Enter </button>
       </div>
     );
   }
