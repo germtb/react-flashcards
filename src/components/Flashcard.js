de08f22e60dff42515381a282fb56f75047f3dcd
germtb <germtf@gmail.com>
2016-03-06 20:48:03 +0000

    Remove unfinished animation

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -3,28 +3,17 @@ import {connect} from 'react-redux';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import {Motion, spring} from 'react-motion';
 
-const outerDivStyle = {
-  display: "flex",
-  justifyContent: "center",
-  WebkitTransform: "translate3d(${x}px, 0, 0)",
-  transform: "translate3d(${x}px, 0, 0)",
-};
-
 const style = {
-  "fontSize": "75",
+  display: 'flex',
+  justifyContent: 'center',
+  fontSize: '75',
 }
 
 export const Flashcard = React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
     return (
-      <Motion style={{x: spring(this.props.question ? 400 : 0)}}>
-          {({x}) =>
-            <div style={outerDivStyle}>
-              <div style={style}> {this.props.question} </div>
-            </div>
-          }
-        </Motion>
+      <div style={style}> {this.props.question} </div>
     );
   }
 });
