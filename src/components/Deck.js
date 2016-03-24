ea27c1a99190c907476900f6670ac1bd3069f695
germtb <germtf@gmail.com>
2016-03-24 00:27:16 +0000

    Implement decks menu

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import {Motion, spring} from 'react-motion';
 
 const deckStyle = {
   justifyContent: 'left',
@@ -15,10 +14,8 @@ const cardStyle = {
 export const Deck = React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
-    var cards = this.props.cards.map(c => (
-        <span style={cardStyle} key={c[0] + c[1]}>
-          {c[0] + ": " + c[1]}
-        </span>
+    const cards = this.props.cards.map(c => (
+        <span style={cardStyle} key={c[0]}> {c[0] + ": " + c[1]} </span>
     ));
     return (<div style={deckStyle}> {cards} </div>);
   }
--- src/components/DecksMenu.js
+++ src/components/DecksMenu.js
@@ -4,22 +4,19 @@ import {FlashcardMenuContainer} from './FlashcardMenu';
 import {connect} from 'react-redux';
 import style from '../style';
 
-export const Body = React.createClass({
+export const Decks = React.createClass({
   mixins: [PureRenderMixin],
   render: function() {
-    switch (this.props.menu) {
-    case 0:
-      return (<FlashcardMenuContainer/>);
-    default:
-      return (<div> NO MENU </div>);
-    }
+    const decks = this.props.decks.map(d => (<Deck cards={d}/>));
+    return (<div {decks} </div>);
+  }
   }
 });
 
 function mapStateToProps(state) {
   return {
-    menu: state.menu
+    decks: state.decks
   };
 }
 
-export const BodyContainer = connect(mapStateToProps)(Body);
+export const DecksContainer = connect(mapStateToProps)(Decks);
